2bfac4ade955680ff81f8c13651cfb7f26c8acb8
Sakib Sarker <95316668+sakibsarker@users.noreply.github.com>
2022-02-17 16:44:12 +0600

    Update 231A.cpp

--- codeforces-c++/231A.cpp
+++ codeforces-c++/231A.cpp
@@ -1,7 +1,15 @@
-
 #include<iostream>
 using namespace std;
 int main(){
-cout<<"Hello World"<<endl;
-return 0;
+    int n,p,v,t,s=0;
+    cin>>n;
+    for (int i = 0; i<n; i++)
+    {
+        cin>>p>>v>>t;
+        if((p==1&&v==1)||(p==1&&t==1)||(t==1&&v==1))
+        s++;
+        }
+        cout<<s<<endl;
+    return 0;
+    
 }
