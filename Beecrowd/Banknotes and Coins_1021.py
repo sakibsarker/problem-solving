6e2e10052b3117b25f9c1d4d91e108ce1c286a4a
Sakib Sarker <95316668+sakibsarker@users.noreply.github.com>
2022-05-21 00:36:09 +0600

    Create Banknotes and Coins_1021.py

---
+++ Beecrowd/Banknotes and Coins_1021.py
@@ -0,0 +1,39 @@
+A=float(input())
+N=A
+a=N/100
+b=N%100
+c=b/50
+d=b%50
+e=d/20
+f=d%20
+g=f/10
+h=f%10
+i=h/5
+j=h%5
+k=j/2
+l=j%2
+E=A*100
+B=(int(E))
+m=B%100
+n=m/50
+o=m%50
+p=o/25
+q=o%25
+r=q/10
+s=q%10
+t=s/5
+u=s%5
+print("NOTAS:")
+print(int(a),"nota(s) de R$ 100.00")
+print(int(c),"nota(s) de R$ 50.00")
+print(int(e),"nota(s) de R$ 20.00")
+print(int(g),"nota(s) de R$ 10.00")
+print(int(i),"nota(s) de R$ 5.00")
+print(int(k),"nota(s) de R$ 2.00")
+print("MOEDAS:")
+print(int(l),"moeda(s) de R$ 1.00")
+print(int(n),"moeda(s) de R$ 0.50")
+print(int(p),"moeda(s) de R$ 0.25")
+print(int(r),"moeda(s) de R$ 0.10")
+print(int(t),"moeda(s) de R$ 0.05")
+print(int(u),"moeda(s) de R$ 0.01")
